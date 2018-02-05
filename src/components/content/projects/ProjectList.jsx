a4920b72b810f75df1abd42988bdf8f2de2a2957
survivorbat <djbatcat@gmail.com>
2018-02-05 10:20:41 +0100

    Changed date sorting

--- src/components/content/projects/ProjectList.jsx
+++ src/components/content/projects/ProjectList.jsx
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 
 import { getProjects } from '../../../actions/projectsActions';
 
+
 class ProjectList extends Component {
     constructor(){
         super();
@@ -19,11 +20,11 @@ class ProjectList extends Component {
             <section>
                 <h4>Current projects:</h4>
                 <div className="card-deck">
-                    {projects.filter(project => project.archived === false).map((project, key) => <Project project={project} key={key}/>)}
+                    {projects.filter(project => project.archived === false).sort((a,b) => {return new Date(a.updated_at)<new Date(b.updated_at)}).map((project, key) => <Project project={project} key={key}/>)}
                 </div>
                 <h4>Archived projects:</h4>
                 <div className="card-deck">
-                    {projects.filter(project => project.archived === true).map((project, key) => <Project project={project} key={key}/>)}
+                    {projects.filter(project => project.archived === true).sort((a,b) => {return new Date(a.updated_at)<new Date(b.updated_at)}).map((project, key) => <Project project={project} key={key}/>)}
                 </div>
             </section>
         );
