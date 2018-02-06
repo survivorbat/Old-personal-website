7a56c02814dacac669d7c1880c3ed263a5a91628
survivorbat <djbatcat@gmail.com>
2018-02-06 10:11:01 +0100

    Added margins

--- src/components/content/projects/Project.jsx
+++ src/components/content/projects/Project.jsx
@@ -4,7 +4,7 @@ class Content extends Component {
   render() {
     const project = this.props.project;
     return (
-      <div className="card">
+      <div className="card" style={{marginTop: "10px", marginBottom:"10px"}}>
         <div className="card-block">
           <h4 className="card-title font-weight-bold"><a href={project.svn_url}>{project.name.replace(/-/g," ")}</a></h4>
           <p className="card-text text-muted"><small>Last updated: {project.updated_at.substring(0,10)}</small></p>
