df2f8357b3dce9711a204909c831147d2c87843a
survivorbat <djbatcat@gmail.com>
2018-02-06 10:03:12 +0100

    Changed card layouts

--- src/components/content/projects/Project.jsx
+++ src/components/content/projects/Project.jsx
@@ -4,18 +4,16 @@ class Content extends Component {
   render() {
     const project = this.props.project;
     return (
-      <div className="col-md-4 col-xs-12 pb-2">
-        <div className="card">
-          <div className="card-block">
-            <h4 className="card-title font-weight-bold"><a href={project.svn_url}>{project.name.replace(/-/g," ")}</a></h4>
-            <p className="card-text text-muted"><small>Last updated: {project.updated_at.substring(0,10)}</small></p>
-            <p className="card-text">{project.description}</p>
-            <p className="card-text"><small>Main language: {project.language}</small></p>
-            {project.homepage!==null && project.homepage!=="" ? <a href={project.homepage}><button className="btn btn-primary"><strong>Demo</strong></button></a> : <div/>}
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Created: {project.created_at.substring(0,10)}</small>
-          </div>
+      <div className="card">
+        <div className="card-block">
+          <h4 className="card-title font-weight-bold"><a href={project.svn_url}>{project.name.replace(/-/g," ")}</a></h4>
+          <p className="card-text text-muted"><small>Last updated: {project.updated_at.substring(0,10)}</small></p>
+          <p className="card-text">{project.description}</p>
+          <p className="card-text"><small>Main language: {project.language}</small></p>
+          {project.homepage!==null && project.homepage!=="" ? <a href={project.homepage}><button className="btn btn-primary"><strong>Demo</strong></button></a> : <div/>}
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">Created: {project.created_at.substring(0,10)}</small>
         </div>
       </div>
     );
--- src/components/content/projects/ProjectList.jsx
+++ src/components/content/projects/ProjectList.jsx
@@ -19,11 +19,11 @@ class ProjectList extends Component {
         return (
             <section>
                 <h4>Current projects:</h4>
-                <div className="card-deck">
+                <div className="card-columns">
                     {projects.filter(project => project.archived === false).sort((a,b) => {return new Date(a.updated_at)<new Date(b.updated_at)}).map((project, key) => <Project project={project} key={key}/>)}
                 </div>
                 <h4>Archived projects:</h4>
-                <div className="card-deck">
+                <div className="card-columns">
                     {projects.filter(project => project.archived === true).sort((a,b) => {return new Date(a.updated_at)<new Date(b.updated_at)}).map((project, key) => <Project project={project} key={key}/>)}
                 </div>
             </section>
