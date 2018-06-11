b0d1749f00792dad92d9d3ccb8158e238d771311
survivorbat <djbatcat@gmail.com>
2018-06-11 09:38:10 +0200

    Added background interactivity

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,7 +23,47 @@ const params = {
       color: "#4fa8f1",
       opacity: 0.8
     },
-  }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 800,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 800,
+        "size": 80,
+        "duration": 2,
+        "opacity": 0.8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 }
 
 class Header extends Component {
